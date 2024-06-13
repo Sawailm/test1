55ff8c3f97e007ee981e50a9017cfe1ff5203ae4
sawailmandhan <sawailmandhan1@gmail.com>
2024-06-13 20:44:18 +0530

    v1

---
+++ Spring-Cart-App/src/test/java/com/example/Spring/Cart/App/SpringCartAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring.Cart.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCartAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
